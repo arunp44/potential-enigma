f2dafe448c9c168e2f7aa318198ebf1cd920935a
arunp44 <137665388+arunp44@users.noreply.github.com>
2024-02-15 12:04:23 -0700

    module9

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,38 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 const questions = [];
+var ans = prompt("1. What is your name?");
+questions.push(ans);
+ans = prompt("2. What is the name of the project?");
+questions.push(ans);
+ans = prompt("3. Name of your github repository?");
+questions.push(ans);
+ans = prompt("4. What is your email?");
+questions.push(ans);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const fs = require('fs');
+    fs.writeFile(fileName, data, (err) => {
+ 
+        // In case of a error throw err.
+        if (err) throw err;
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerAnswers) => {
+        console.log("Generating.....");
+        writeToFile("./utils/README.md", generateMarkdown({ ...inquirerAnswers }));
+    })
+}
 
 // Function call to initialize app
 init();
+
 
